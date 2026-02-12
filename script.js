69fbf5a721fd33261df16776eb9fa1b8889d31fa
Issysk1 <ismailcheikh6@gmail.com>
2026-02-12 10:34:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -74,14 +74,15 @@ const questions = [
     ]
   },
   {
-    q: "Who fell in love first? 💘",
-    a: [
-      ["You (I saw it coming)", "I noticed before you admitted it… and I loved that 🔥", false],
-      ["Both at the same time (soulmate timing ✨)", "", true],
-      ["Me (I tried to play it cool)", "I absolutely failed at hiding it, didn’t I? 💗", false],
-      ["Our vibes before we did", "Honestly yeah, everyone else knew before we did 🐝", false]
-    ]
-  },
+  q: "Who fell in love first? 💘",
+  a: [
+    ["You (but I fell harder 💖)", "I noticed before you admitted it… and I loved that ", true],
+    ["Both at the same time (soulmate timing ✨)", "", false],
+    ["Me (I tried to play it cool)", "I absolutely failed at hiding it, didn’t I? 💗", false],
+    ["Our vibes before we did", "Honestly yeah, everyone else knew before we did 🐝", false]
+  ]
+}
+
   {
     q: "Who is funnier? 😏",
     a: [
